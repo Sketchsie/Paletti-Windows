42d30036118389e2d96e8f976d4a3e060c538ecf
Eroica <seba79@mailbox.org>
2020-06-07 11:34:55 +0800

    Add icon to stage
    jpackage adds an ICO to the executable, but without this there is no
    icon in Window's Task View.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
+import javafx.scene.image.Image
 import javafx.stage.Stage
 import javafx.stage.StageStyle
 
@@ -11,6 +12,7 @@ class Paletti : Application() {
     override fun start(primaryStage: Stage) {
         val stage = FXMLLoader.load<Stage>(javaClass.getResource("MainWindow.fxml"))
         stage.initStyle(StageStyle.TRANSPARENT)
+        stage.icons += Image(javaClass.getResourceAsStream("icons/256.png"))
         stage.focusedProperty().addListener { _, _, hasFocus ->
             if (hasFocus) {
                 stage.scene.root.styleClass.remove("paletti-no-focus")
